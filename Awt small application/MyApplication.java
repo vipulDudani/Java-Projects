aa02dfa55771fb13b8d0d225e6e7c2ec0532e3fe
vipulDudani <128981674+vipulDudani@users.noreply.github.com>
2023-04-27 16:38:15 +0530

    Add files via upload

---
+++ Awt small application/MyApplication.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class MyApplication extends Frame {
+
+    public MyApplication() {
+        Button button = new Button("Click me!");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Button clicked!");
+            }
+        });
+        add(button);
+        setSize(200, 200);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MyApplication();
+    }
+
+}
